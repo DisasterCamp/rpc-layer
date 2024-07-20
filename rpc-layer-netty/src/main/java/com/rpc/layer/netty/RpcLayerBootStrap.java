d857ccb80d6e8673a95104aba69dd543d05d1182
Disaster <58838568+disaster1-tesk@users.noreply.github.com>
2024-07-20 11:14:56 +0800

    :busts_in_silhouette: Update pom
    pom modify

--- rpc-layer-netty/src/main/java/com/rpc/layer/netty/RpcLayerBootStrap.java
+++ rpc-layer-netty/src/main/java/com/rpc/layer/netty/RpcLayerBootStrap.java
@@ -1,12 +1,13 @@
 package com.rpc.layer.netty;
 
+import com.rpc.layer.base.enums.BootStrapType;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class RpcLayerBootStrap {
     private final AtomicBoolean clientIsRunning = new AtomicBoolean(false);
     private final AtomicBoolean serverIsRunning = new AtomicBoolean(false);
 
-    public static void init(){
+    public static void init(BootStrapType bootStrapType){
 
     }
 }
